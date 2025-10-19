fe275215d9ef164456156b36cf970526c023b667
hansu88 <gimhans0917@gmail.com>
2025-10-19 09:53:37 +0900

    feat: Controller에 포인트 조회 기능 연결 및 final 적용

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -11,15 +11,22 @@ import java.util.List;
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
+
+    public PointController(PointService pointService) {
+        this.pointService = pointService;
+    }
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     * 어떠한 유저를 반환해도 ID도 0, 포인트를 조회하지않아서 처리
      */
     @GetMapping("{id}")
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+//        return new UserPoint(0, 0, 0);
+        return pointService.getUserPoint(id);
     }
 
     /**
