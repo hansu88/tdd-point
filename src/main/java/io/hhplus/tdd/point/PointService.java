bebe88e6c7aad2eba8ee1b82a59957e6ace89298
hansu88 <gimhans0917@gmail.com>
2025-10-19 09:53:25 +0900

    feat: 포인트 조회 서비스 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,4 +1,21 @@
 package io.hhplus.tdd.point;
 
-public class PointServiceTest {
+import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PointService {
+
+    private final UserPointTable userPointTable;
+
+    public PointService(UserPointTable userPointTable) {
+        this.userPointTable = userPointTable;
+    }
+
+    /**
+     * 특정 유저의 포인트를 조회한다
+     */
+    public UserPoint getUserPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
 }
