08d022f5aa59826c3e94667e6330e6b8c67cfba9
hansu88 <gimhans0917@gmail.com>
2025-10-19 09:56:25 +0900

    test: postman 체크를 위한 임시테스트

--- src/main/java/io/hhplus/tdd/TddApplication.java
+++ src/main/java/io/hhplus/tdd/TddApplication.java
@@ -1,12 +1,26 @@
 package io.hhplus.tdd;
 
+import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TddApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TddApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initData(UserPointTable userPointTable) {
+        return args -> {
+            // 테스트용 초기 데이터
+            userPointTable.insertOrUpdate(1L, 1000L);
+            userPointTable.insertOrUpdate(2L, 0L);
+            userPointTable.insertOrUpdate(3L, 0L);
+            System.out.println("✅ 테스트 데이터 초기화 완료!");
+        };
     }
 }
