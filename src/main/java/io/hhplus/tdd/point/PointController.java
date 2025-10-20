a056b256b0862b601844fbc4f60453e1b826d2e9
hansu88 <gimhans0917@gmail.com>
2025-10-20 21:48:18 +0900

    feat: Controller에 포인트 사용 기능 연결

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -59,6 +59,7 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+//        return new UserPoint(0, 0, 0);
+        return pointService.usePoint(id, amount);
     }
 }
