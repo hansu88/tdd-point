8e8159fe04bca8d0944201d7b620e05de03e1013
hansu88 <gimhans0917@gmail.com>
2025-10-23 12:14:55 +0900

    (레드) 포인트 사용 Controller 테스트 작성

---
+++ src/test/java/io/hhplus/tdd/point/PointUseControllerTest.java
@@ -0,0 +1,47 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PointController.class)
+public class PointUseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PointService pointService;
+
+    @Test
+    @DisplayName("포인트 사용 API 호출시 차감된 포인트를 반환합니다")
+    void usePoint_SuccessValue() throws Exception {
+        // given
+        long userId = 1L;
+        long useAmount = 3000;
+        UserPoint expectedPoint = new UserPoint(userId, 7000L , System.currentTimeMillis());
+
+        when(pointService.usePoint(anyLong(), anyLong())).thenReturn(expectedPoint);
+
+        // when & then
+        mockMvc.perform(patch("/point/{id}/use",userId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(String.valueOf(useAmount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.point").value(expectedPoint));
+    }
+}
